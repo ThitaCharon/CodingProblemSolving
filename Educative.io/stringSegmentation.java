220994e8cd46bf4bcd21bc1e64ca77cf02158618
Thita Charoentham <thita.charoentham35@qmail.cuny.edu>
2019-04-09 23:40:49 -0400

    stringSegmentation

---
+++ Educative.io/stringSegmentation.java
@@ -0,0 +1,10 @@
+class stringSegmentation {
+    public static boolean can_segment_string(String s, Set<String> dict) {
+        // TODO: Write - Your - Code
+        for (String str : dict) {
+            System.out.print(str + ", ");
+        }
+
+        return false;
+    }
+}
